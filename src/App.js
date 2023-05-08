1efe8ab1701ef5044389edd1351ce4f1cf0bfc65
Olga Shokalo <olha.shokalo@gmail.com>
2023-05-08 13:25:26 +0300

    Updated project link location

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Weather from "./Weather.js";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer>
+          <a href="https://github.com/olgashokalo/Weather-App-React">
+            Open-source code
+          </a>
+          by Olga Shokalo
+        </footer>
+      </div>
     </div>
   );
 }
